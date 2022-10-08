4a6b6df7f37b2fce7474a78a5fc19f7325e5b3a3
wangsun <825678776@qq.com>
2022-10-08 21:58:05 +0800

    add daily problem 2022/10/08

---
+++ all-code/801-900/870advantageCount.py
@@ -0,0 +1,49 @@
+# 给定两个大小相等的数组 nums1 和 nums2，nums1 相对于 nums2 的优势可以用满足 nums1[i] > nums2[i] 的索引 i 的数目来描述。
+#
+# 返回 nums1 的任意排列，使其相对于 nums2 的优势最大化。
+#
+#  
+#
+# 示例 1：
+#
+# 输入：nums1 = [2,7,11,15], nums2 = [1,10,4,11]
+# 输出：[2,11,7,15]
+# 示例 2：
+#
+# 输入：nums1 = [12,24,8,32], nums2 = [13,25,32,11]
+# 输出：[24,32,8,12]
+#  
+#
+# 提示：
+#
+# 1 <= nums1.length <= 105
+# nums2.length == nums1.length
+# 0 <= nums1[i], nums2[i] <= 109
+#
+# https://leetcode.cn/problems/advantage-shuffle
+
+
+from typing import List
+import bisect
+import collections
+class Solution:
+    def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort(reverse=True)
+        n = len(nums2)
+        n2 = sorted(zip(nums2, range(n)), reverse=True)
+        ans = [0] * n
+        i, k = 0, n - 1
+        for j in range(n):
+            if n2[j][0] < nums1[i]:
+                ans[n2[j][1]] = nums1[i]
+                i += 1
+            else:
+                ans[n2[j][1]] = nums1[k]
+                k -= 1
+        return ans
+
+
+so = Solution()
+print(so.advantageCount(nums1 = [2,7,11,15], nums2 = [1,10,4,11]))  # [2,11,7,15]
+print(so.advantageCount(nums1 = [12,24,8,32], nums2 = [13,25,32,11]))
+
