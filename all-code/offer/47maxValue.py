757ace9b87652793afb37589b400f24f4a4d436d
wangsun <825678776@qq.com>
2023-03-08 22:42:59 +0800

    add daily problem 2023/3/8

---
+++ all-code/offer/47maxValue.py
@@ -0,0 +1,80 @@
+# 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+#
+#
+#
+# 示例 1:
+#
+# 输入:
+# [
+#   [1,3,1],
+#   [1,5,1],
+#   [4,2,1]
+# ]
+# 输出: 12
+# 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+#
+#
+# 提示：
+#
+# 0 < grid.length <= 200
+# 0 < grid[0].length <= 200
+
+
+from typing import List
+from collections import deque
+# Definition for a binary tree node.
+from collections import Counter
+from collections import defaultdict
+# d = Counter(list1)
+# d = defaultdict(int)
+#import random
+# random.uniform(a, b)，用于生成一个指定范围内的随机浮点数，闭区间
+# randint和randrange的区别：
+# randint 产生的随机数区间是包含左右极限的，也就是说左右都是闭区间的[1, n]，能取到1和n。
+# 而 randrange 产生的随机数区间只包含左极限，也就是左闭右开的[1, n)，1能取到，而n取不到。
+
+# 浮点数： price = "{:.02f}".format(price)
+
+import bisect
+# bisect_right：
+# 若序列a中存在与x相同的元素，则返回x相等元素右侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x左侧距离最近元素插入点的索引位置
+# pos = bisect.bisect_right(left, tail)
+# bisect_left：
+# 若序列a中存在与x相同的元素，则返回x相等元素左侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x右侧距离最近元素插入点的索引位置
+
+# Map = [['U' for _ in range(n)] for _ in range(m)]
+
+from functools import lru_cache
+from typing import List
+# @lru_cache(None)
+
+
+
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        r, c = len(grid), len(grid[0])
+        dp = [[0] * c for _ in range(r)]
+        # dp[0][0] = grid[0][0]
+        for i in range(r):
+            for j in range(c):
+                if i > 0:
+                    dp[i][j] = dp[i - 1][j]
+                if j > 0:
+                    dp[i][j] = max(dp[i][j - 1], dp[i][j])
+                dp[i][j] += grid[i][j]
+        return dp[-1][-1]
+
+
+
+so = Solution()
+print(so.maxValue([
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]))
+
+
+
+
