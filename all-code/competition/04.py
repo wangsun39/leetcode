c001937b97c68e2edf2a5e5f0af769f2bbfa4846
wangsun <825678776@qq.com>
2023-03-18 08:10:53 +0800

    add daily problem 2023/3/18

--- all-code/competition/04.py
+++ all-code/competition/04.py
@@ -1,3 +1,37 @@
+# 你有一台电脑，它可以 同时 运行无数个任务。给你一个二维整数数组 tasks ，其中 tasks[i] = [starti, endi, durationi] 表示第 i 个任务需要在 闭区间 时间段 [starti, endi] 内运行 durationi 个整数时间点（但不需要连续）。
+#
+# 当电脑需要运行任务时，你可以打开电脑，如果空闲时，你可以将电脑关闭。
+#
+# 请你返回完成所有任务的情况下，电脑最少需要运行多少秒。
+#
+#
+#
+# 示例 1：
+#
+# 输入：tasks = [[2,3,1],[4,5,1],[1,5,2]]
+# 输出：2
+# 解释：
+# - 第一个任务在闭区间 [2, 2] 运行。
+# - 第二个任务在闭区间 [5, 5] 运行。
+# - 第三个任务在闭区间 [2, 2] 和 [5, 5] 运行。
+# 电脑总共运行 2 个整数时间点。
+# 示例 2：
+#
+# 输入：tasks = [[1,3,2],[2,5,3],[5,6,2]]
+# 输出：4
+# 解释：
+# - 第一个任务在闭区间 [2, 3] 运行
+# - 第二个任务在闭区间 [2, 3] 和 [5, 5] 运行。
+# - 第三个任务在闭区间 [5, 6] 运行。
+# 电脑总共运行 4 个整数时间点。
+#
+#
+# 提示：
+#
+# 1 <= tasks.length <= 2000
+# tasks[i].length == 3
+# 1 <= starti, endi <= 2000
+# 1 <= durationi <= endi - starti + 1
 
 from typing import List
 from typing import Optional
@@ -107,29 +141,19 @@ from sortedcontainers import SortedList
 
 class Solution:
     def findMinimumTime(self, tasks: List[List[int]]) -> int:
-        tasks.sort()
-        n = len(tasks)
-        counter = Counter()
-        for x, y, d in tasks:
-            for i in range(x, y + 1):
-                counter[i] += 1
-
-        ans = set()
-        while len(counter):
-            k, v = counter.most_common(1)[0]
-            ans.add(k)
-            counter = Counter()
-            for i in range(n):
-                if tasks[i][0] <= k <= tasks[i][1]:
-                    tasks[i][2] -= 1
-                    if tasks[i][2] == 0:
-                        continue
-                for j in range(tasks[i][0], tasks[i][1] + 1):
-                    if j not in ans:
-                        counter[j] += 1
-
-        return len(ans)
-
+        tasks.sort(key=lambda x: x[1])
+        vis = set()
+        # print(tasks)
+        for l, r, d in tasks:
+            cand = []
+            for i in range(l, r + 1):
+                if i not in vis:
+                    cand.append(i)
+            need = d - (r - l + 1 - len(cand))
+            for i in range(need):
+                vis.add(cand[-i - 1])
+        # print(vis)
+        return len(vis)
 
 
 so = Solution()
