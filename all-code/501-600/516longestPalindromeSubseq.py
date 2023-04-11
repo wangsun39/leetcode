63d52a21044dc5bca0273c059eb4f923324ffb7c
wangsun <825678776@qq.com>
2023-04-11 21:53:37 +0800

    add daily problem 2023/4/11

--- all-code/501-600/516longestPalindromeSubseq.py
+++ all-code/501-600/516longestPalindromeSubseq.py
@@ -21,9 +21,10 @@
 # 1 <= s.length <= 1000
 # s 仅由小写英文字母组成
 
+from functools import cache
 
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
+    def longestPalindromeSubseq1(self, s: str) -> int:
         N = len(s)
         dp = [[0] * N for _ in range(N)]  # dp[i][j] 表示 在区间s[i]...s[j]内最长回文子序列
         for i in range(N):
@@ -40,6 +41,40 @@ class Solution:
 
         print(dp)
         return dp[0][N - 1]
+
+    def longestPalindromeSubseq2(self, s: str) -> int:
+        # 2023/4/11 等价于：求原串和逆序串的LCS
+        s1, s2 = s, s[::-1]
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if s1[i] == s2[j]:
+                    if i > 0 and j > 0:
+                        dp[i][j] = dp[i - 1][j - 1] + 1
+                    else:
+                        dp[i][j] = 1
+                else:
+                    if i > 0:
+                        dp[i][j] = dp[i - 1][j]
+                    if j > 0:
+                        dp[i][j] = max(dp[i][j], dp[i][j - 1])
+        return dp[-1][-1]
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # 2023/4/11 区间DP的DFS版本，这个性能高
+        n = len(s)
+
+        @cache
+        def dfs(i, j):  # s[i: j + 1] 的最长回文子序列
+            if i > j: return 0
+            if i == j: return 1
+            if s[i] == s[j]:
+                return dfs(i + 1, j - 1)
+            return max(dfs(i + 1, j), dfs(i, j - 1))
+
+        return dfs(0, n - 1)
+
 
 so = Solution()
 print(so.longestPalindromeSubseq("bbbab"))
