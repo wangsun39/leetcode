c90c881c56eaaeb26db4f351b66697426cf17945
wangsun <825678776@qq.com>
2023-06-04 20:51:20 +0800

    add daily problem 2023/6/4 & weekly contest 348

---
+++ all-code/2401-2500/2465distinctAverages.py
@@ -0,0 +1,153 @@
+# 给你一个下标从 0 开始长度为 偶数 的整数数组 nums 。
+#
+# 只要 nums 不是 空数组，你就重复执行以下步骤：
+#
+# 找到 nums 中的最小值，并删除它。
+# 找到 nums 中的最大值，并删除它。
+# 计算删除两数的平均值。
+# 两数 a 和 b 的 平均值 为 (a + b) / 2 。
+#
+# 比方说，2 和 3 的平均值是 (2 + 3) / 2 = 2.5 。
+# 返回上述过程能得到的 不同 平均值的数目。
+#
+# 注意 ，如果最小值或者最大值有重复元素，可以删除任意一个。
+#
+#
+#
+# 示例 1：
+#
+# 输入：nums = [4,1,4,0,3,5]
+# 输出：2
+# 解释：
+# 1. 删除 0 和 5 ，平均值是 (0 + 5) / 2 = 2.5 ，现在 nums = [4,1,4,3] 。
+# 2. 删除 1 和 4 ，平均值是 (1 + 4) / 2 = 2.5 ，现在 nums = [4,3] 。
+# 3. 删除 3 和 4 ，平均值是 (3 + 4) / 2 = 3.5 。
+# 2.5 ，2.5 和 3.5 之中总共有 2 个不同的数，我们返回 2 。
+# 示例 2：
+#
+# 输入：nums = [1,100]
+# 输出：1
+# 解释：
+# 删除 1 和 100 后只有一个平均值，所以我们返回 1 。
+#
+#
+# 提示：
+#
+# 2 <= nums.length <= 100
+# nums.length 是偶数。
+# 0 <= nums[i] <= 100
+
+import copy
+from typing import List
+from typing import Optional
+from cmath import inf
+from collections import deque
+# de = deque([1, 2, 3])
+# de.append(4)
+# de.appendleft(6)
+# de.pop()
+# de.popleft()
+from itertools import pairwise
+# Definition for a binary tree node.
+from collections import Counter
+from collections import defaultdict
+# d = Counter(list1)
+# d = defaultdict(int)
+import math
+import random
+# random.uniform(a, b)，用于生成一个指定范围内的随机浮点数，闭区间
+# randint和randrange的区别：
+# randint 产生的随机数区间是包含左右极限的，也就是说左右都是闭区间的[1, n]，能取到1和n。
+# 而 randrange 产生的随机数区间只包含左极限，也就是左闭右开的[1, n)，1能取到，而n取不到。
+
+# 浮点数： price = "{:.02f}".format(price)
+# newword = float(word[1:]) * (100 - discount) / 100
+# newword = "%.2f" % newword
+# a.isalpha()  # 判断字符串中是否所有的字符都是字母
+# a.isdigit() # 判断字符串中是否所有的字符都是整数
+# a.isalnum()  # 判断字符串中是否所有的字符都是字母or数字
+# a.isspace()  # 判断字符串中是否所有的字符都是空白符
+# a.swapcase()  # 转换大小写
+
+import bisect
+# bisect_right：
+# 若序列a中存在与x相同的元素，则返回x相等元素右侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x左侧距离最近元素插入点的索引位置
+# pos = bisect.bisect_right(left, tail)
+# bisect_left：
+# 若序列a中存在与x相同的元素，则返回x相等元素左侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x右侧距离最近元素插入点的索引位置
+import heapq
+# heap.heapify(nums) # 小顶堆
+# heapq.heappop() 函数弹出堆中最小值
+# heapq.heappush(nums, 1)
+# heapq.heapreplace(heap, item)  删除最小值并添加新值
+# 如果需要获取堆中最大或最小的范围值，则可以使用heapq.nlargest() 或heapq.nsmallest() 函数
+
+# Map = [['U' for _ in range(n)] for _ in range(m)]
+
+from functools import lru_cache, cache
+from typing import List
+# @lru_cache(None)
+
+# bit位 函数：
+# n.bit_length()  数值的二进制的长度数
+# value = int(s, 2)
+# lowbit(i) 即i&-i	返回i的最后一位1
+# n>>k & 1	求n的第k位数字
+# x | (1 << k)	将x第k位 置为1
+# x ^ (1 << k)	将x第k位取反
+# x & (x - 1)	将x最右边的1置为0(去掉最右边的1)
+# x | (x + 1)	将x最右边的0置为1
+# x & 1	判断奇偶性 真为奇，假为偶
+
+
+import string
+# string.digits  表示 0123456789
+# string.letters：包含所有字母(大写或小写字符串，在python3.0中，使用string.ascii-letters代替)
+# string.ascii_lowercase：包含所有小写字母的字符串
+# string.printable：包含所有可打印字符的字符串
+# string.punctuation：包含所有标点的字符串
+# string.uppercase：包含所有大写字母的字符串
+
+# f-string用法
+# name = 'sun'
+# f"Hello, my name is {name}"
+
+from itertools import accumulate
+# s = list(accumulate(nums, initial=0))  # 计算前缀和
+
+from sortedcontainers import SortedList
+    # SortedList.add(value) 添加新元素，并排序。时间复杂度O(log(n)).
+    # SortedList.update(iterable) 对添加的可迭代的所有元素排序。时间复杂度O(k*log(n)).
+    # SortedList.clear() 移除所有元素。时间复杂度O(n).
+    # SortedList.discard(value) 移除一个值元素，如果元素不存在，不报错。时间复杂度O(log(n)).
+    # SortedList.remove(value) 移除一个值元素，如果元素不存在，报错ValueError。时间复杂度O(log(n)).
+    # SortedList.pop(index=-1) 移除一个指定下标元素，如果有序序列为空或者下标超限，报错IndexError.
+    # SortedList.bisect_left(value)
+    # SortedList.bisect_right(value)
+    # SortedList.count(value)
+    # SortedList.index(value, start=None, Stop=None) 查找索引范围[start,stop）内第一次出现value的索引，如果value不存在，报错ValueError.
+
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        s = set()
+        for i in range(n // 2):
+            a, b = nums[0], nums[-1]
+            s.add((a + b) / 2)
+            nums.pop(0)
+            nums.pop()
+        return len(s)
+
+
+
+so = Solution()
+print(so.distinctAverages(nums = [9,5,7,8,7,9,8,2,0,7]))  # 5
+print(so.distinctAverages(nums = [4,1,4,0,3,5]))  # 2
+print(so.distinctAverages(nums = [1,100]))  # 1
+
+
+
+
--- all-code/competition/04.py
+++ all-code/competition/04.py
@@ -1,4 +1,29 @@
-
+# 给你两个数字字符串 num1 和 num2 ，以及两个整数 max_sum 和 min_sum 。如果一个整数 x 满足以下条件，我们称它是一个好整数：
+#
+# num1 <= x <= num2
+# min_sum <= digit_sum(x) <= max_sum.
+# 请你返回好整数的数目。答案可能很大，请返回答案对 109 + 7 取余后的结果。
+#
+# 注意，digit_sum(x) 表示 x 各位数字之和。
+#
+#
+#
+# 示例 1：
+#
+# 输入：num1 = "1", num2 = "12", min_num = 1, max_num = 8
+# 输出：11
+# 解释：总共有 11 个整数的数位和在 1 到 8 之间，分别是 1,2,3,4,5,6,7,8,10,11 和 12 。所以我们返回 11 。
+# 示例 2：
+#
+# 输入：num1 = "1", num2 = "5", min_num = 1, max_num = 5
+# 输出：5
+# 解释：数位和在 1 到 5 之间的 5 个整数分别为 1,2,3,4 和 5 。所以我们返回 5 。
+#
+#
+# 提示：
+#
+# 1 <= num1 <= num2 <= 1022
+# 1 <= min_sum <= max_sum <= 400
 from typing import List
 from typing import Optional
 from cmath import inf
@@ -126,12 +151,43 @@ from sortedcontainers import SortedList, SortedDict, SortedSet
 # list(zip(*nums))  # [(7, 6, 6, 3), (2, 4, 5, 2), (1, 2, 3, 1)]    转置
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def count(self, num1: str, num2: str, min_sum: int, max_sum: int) -> int:
+        MOD = 10 ** 9 + 7
+        @cache
+        def helper(upper, limit):  # 字符串 upper
+            if len(upper) == 0:
+                return 0 == limit
+            first = int(upper[0])
+            if first <= limit:
+                res = 0
+                for i in range(first):
+                    res += helper('9' * (len(upper) - 1), limit - i)
+                    res %= MOD
+                res += helper(upper[1:], limit - first)
+                res %= MOD
+                return res
+            res = 0
+            for i in range(limit + 1):
+                res += helper('9' * (len(upper) - 1), limit - i)
+                res %= MOD
+            return res
+
+        def f(upper):
+            ans = 0
+            for lim in range(min_sum, min(22 * 9 + 1, max_sum + 1)):
+                ans += helper(upper, lim)
+                ans %= MOD
+            # print(upper, ans)
+            return ans
+        num1 = str(int(num1) - 1)
+        return (f(num2) + MOD - f(num1)) % MOD
+
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.count("1000000007","2000000014",1,400))  # 1
+print(so.count("1", "12", 1, 8))  # 11
+print(so.count(num1 = "1", num2 = "5", min_sum = 1, max_sum = 5))
 
 
 
