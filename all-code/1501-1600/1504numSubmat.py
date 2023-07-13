d1bfec74f1eea76d9b43a525e5d50281de497a1a
wangsun <825678776@qq.com>
2023-07-13 23:10:39 +0800

    add daily problem 2023/7/13

---
+++ all-code/1501-1600/1504numSubmat.py
@@ -0,0 +1,84 @@
+# 给你一个 m x n 的二进制矩阵 mat ，请你返回有多少个 子矩形 的元素全部都是 1 。
+#
+#
+#
+# 示例 1：
+#
+#
+#
+# 输入：mat = [[1,0,1],[1,1,0],[1,1,0]]
+# 输出：13
+# 解释：
+# 有 6 个 1x1 的矩形。
+# 有 2 个 1x2 的矩形。
+# 有 3 个 2x1 的矩形。
+# 有 1 个 2x2 的矩形。
+# 有 1 个 3x1 的矩形。
+# 矩形数目总共 = 6 + 2 + 3 + 1 + 1 = 13 。
+# 示例 2：
+#
+#
+#
+# 输入：mat = [[0,1,1,0],[0,1,1,1],[1,1,1,0]]
+# 输出：24
+# 解释：
+# 有 8 个 1x1 的子矩形。
+# 有 5 个 1x2 的子矩形。
+# 有 2 个 1x3 的子矩形。
+# 有 4 个 2x1 的子矩形。
+# 有 2 个 2x2 的子矩形。
+# 有 2 个 3x1 的子矩形。
+# 有 1 个 3x2 的子矩形。
+# 矩形数目总共 = 8 + 5 + 2 + 4 + 2 + 2 + 1 = 24 。
+#
+#
+#
+# 提示：
+#
+# 1 <= m, n <= 150
+# mat[i][j] 仅包含 0 或 1
+
+
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+from typing import List
+
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        r, c = len(mat), len(mat[0])
+        right = [[0] * c for _ in range(r)]  # 记录每个位置及后侧有多少个连续1
+        for i in range(r):
+            stack = []
+            for j in range(c + 1):
+                if j < c and mat[i][j] == 1:
+                    stack.append(j)
+                else:
+                    while stack:
+                        k = stack.pop()
+                        right[i][k] = j - k
+        # print(right)
+        ans = 0
+        for i in range(r):
+            for j in range(c):
+                max_width = right[i][j]
+                for k in range(i, r):
+                    max_width = min(max_width, right[k][j])
+                    if max_width == 0: break
+                    ans += max_width
+        return ans
+
+
+
+
+
+
+
+so = Solution()
+print(so.numSubmat([[1,0,1],[1,1,0],[1,1,0]]))
+print(so.numSubmat(mat = [[0,1,1,0],[0,1,1,1],[1,1,1,0]]))
+
+
+
+
+
---
+++ all-code/900-999/931minFallingPathSum.py
@@ -0,0 +1,47 @@
+# 给你一个 n x n 的 方形 整数数组 matrix ，请你找出并返回通过 matrix 的下降路径 的 最小和 。
+#
+# 下降路径 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。具体来说，位置 (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1) 。
+#
+#
+#
+# 示例 1：
+#
+#
+#
+# 输入：matrix = [[2,1,3],[6,5,4],[7,8,9]]
+# 输出：13
+# 解释：如图所示，为和最小的两条下降路径
+# 示例 2：
+#
+#
+#
+# 输入：matrix = [[-19,57],[-40,-5]]
+# 输出：-59
+# 解释：如图所示，为和最小的下降路径
+#
+#
+# 提示：
+#
+# n == matrix.length == matrix[i].length
+# 1 <= n <= 100
+# -100 <= matrix[i][j] <= 100
+from cmath import inf
+from typing import List
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        r, c = len(matrix), len(matrix[0])
+        dp1, dp2 = matrix[0][:], [-inf] * c
+        for i in range(1, r):
+            for j in range(c):
+                pre = dp1[j]
+                if j:
+                    pre = min(pre, dp1[j - 1])
+                if j < c - 1:
+                    pre  = min(pre, dp1[j + 1])
+                dp2[j] = pre + matrix[i][j]
+            dp1, dp2 = dp2, [-inf] * c
+        return min(dp1)
+
+
+so = Solution()
