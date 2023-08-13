3a4292c9b5225da9f74cf240ada1c77f80e605e6
wangsun <825678776@qq.com>
2023-08-13 10:13:25 +0800

    add daily problem 2023/8/13

--- all-code/0-100/088merge.py
+++ all-code/0-100/088merge.py
@@ -18,6 +18,24 @@ class Solution:
                 p1 -= 1
         return
 
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        2023/8/13
+        """
+        cur = m + n - 1
+        p1, p2 = m - 1, n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[cur] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[cur] = nums1[p1]
+                p1 -= 1
+            cur -= 1
+        if p2 >= 0:
+            nums1[: p2 + 1] = nums2[: p2 + 1]
+        return
 
 so = Solution()
 nums1 = [1,2,3,0,0,0]
