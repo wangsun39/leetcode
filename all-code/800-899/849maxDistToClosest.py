2a387e7bee17b01190143d7393ed6d3ea4a3ebe8
wangsun <825678776@qq.com>
2023-08-22 22:14:31 +0800

    add daily problem & update weekly contest 359

---
+++ all-code/800-899/849maxDistToClosest.py
@@ -0,0 +1,62 @@
+# 给你一个数组 seats 表示一排座位，其中 seats[i] = 1 代表有人坐在第 i 个座位上，seats[i] = 0 代表座位 i 上是空的（下标从 0 开始）。
+#
+# 至少有一个空座位，且至少有一人已经坐在座位上。
+#
+# 亚历克斯希望坐在一个能够使他与离他最近的人之间的距离达到最大化的座位上。
+#
+# 返回他到离他最近的人的最大距离。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：seats = [1,0,0,0,1,0,1]
+# 输出：2
+# 解释：
+# 如果亚历克斯坐在第二个空位（seats[2]）上，他到离他最近的人的距离为 2 。
+# 如果亚历克斯坐在其它任何一个空位上，他到离他最近的人的距离为 1 。
+# 因此，他到离他最近的人的最大距离是 2 。
+# 示例 2：
+#
+# 输入：seats = [1,0,0,0]
+# 输出：3
+# 解释：
+# 如果亚历克斯坐在最后一个座位上，他离最近的人有 3 个座位远。
+# 这是可能的最大距离，所以答案是 3 。
+# 示例 3：
+#
+# 输入：seats = [0,1]
+# 输出：1
+#
+#
+# 提示：
+#
+# 2 <= seats.length <= 2 * 104
+# seats[i] 为 0 或 1
+# 至少有一个 空座位
+# 至少有一个 座位上有人
+
+import collections
+from typing import List
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        ans = 0
+        pre = -1
+        for i, x in enumerate(seats):
+            if x == 0: continue
+            if pre == -1:
+                ans = i
+            else:
+                ans = max(ans, (i - pre) // 2)
+            pre = i
+        return max(ans, len(seats) - pre - 1)
+
+
+
+so = Solution()
+print(so.maxDistToClosest([1,0,0,1]))
+print(so.maxDistToClosest([1,0,0,0]))
+print(so.maxDistToClosest([1,0,0,0,1,0,1]))
+print(so.maxDistToClosest([0,1]))
+
