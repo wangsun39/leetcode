42375edf729af48fee2890372cabf9c769796d3e
wangsun <825678776@qq.com>
2023-08-31 22:48:30 +0800

    add daily problem 2023/8/23

---
+++ all-code/1600-1699/1654minimumJumps.py
@@ -0,0 +1,85 @@
+# 有一只跳蚤的家在数轴上的位置 x 处。请你帮助它从位置 0 出发，到达它的家。
+#
+# 跳蚤跳跃的规则如下：
+#
+# 它可以 往前 跳恰好 a 个位置（即往右跳）。
+# 它可以 往后 跳恰好 b 个位置（即往左跳）。
+# 它不能 连续 往后跳 2 次。
+# 它不能跳到任何 forbidden 数组中的位置。
+# 跳蚤可以往前跳 超过 它的家的位置，但是它 不能跳到负整数 的位置。
+#
+# 给你一个整数数组 forbidden ，其中 forbidden[i] 是跳蚤不能跳到的位置，同时给你整数 a， b 和 x ，请你返回跳蚤到家的最少跳跃次数。如果没有恰好到达 x 的可行方案，请你返回 -1 。
+#
+#
+#
+# 示例 1：
+#
+# 输入：forbidden = [14,4,18,1,15], a = 3, b = 15, x = 9
+# 输出：3
+# 解释：往前跳 3 次（0 -> 3 -> 6 -> 9），跳蚤就到家了。
+# 示例 2：
+#
+# 输入：forbidden = [8,3,16,6,12,20], a = 15, b = 13, x = 11
+# 输出：-1
+# 示例 3：
+#
+# 输入：forbidden = [1,6,2,14,5,17,4], a = 16, b = 9, x = 7
+# 输出：2
+# 解释：往前跳一次（0 -> 16），然后往回跳一次（16 -> 7），跳蚤就到家了。
+#
+#
+# 提示：
+#
+# 1 <= forbidden.length <= 1000
+# 1 <= a, b, forbidden[i] <= 2000
+# 0 <= x <= 2000
+# forbidden 中所有位置互不相同。
+# 位置 x 不在 forbidden 中。
+from math import gcd
+from typing import Optional,List
+from collections import defaultdict, deque
+
+
+# Definition for a binary tree node.
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        if x == 0: return 0
+        g = gcd(a, b)
+        if x % g != 0:
+            return -1
+        vis = {}
+        for y in forbidden:
+            vis[y] = 2  # 1 表示之前是从dir == -1到达的之后还能往左走， 2表示之之前是从dir == 1到达的，之后不能再访问了
+        dq1 = deque([[0, 0]])
+        cnt = 1
+        while dq1:
+            dq2 = deque()
+            while dq1:
+                y, d = dq1.popleft()
+                u, v = y + a, y - b
+                if u == x:
+                    return cnt
+                if (u not in vis or vis[u] == 1) and u < 6000:
+                    dq2.append([u, 1])
+                    vis[u] = 2
+                if d >= 0:
+                    if v == x:
+                        return cnt
+                    if v > 0 and v not in vis:
+                        dq2.append([v, -1])
+                        vis[v] = 1
+            cnt += 1
+            dq1 = dq2
+        return -1
+
+so = Solution()
+print(so.minimumJumps(forbidden = [8,3,16,6,12,20], a = 15, b = 13, x = 11))   # -1
+print(so.minimumJumps(forbidden = [162,118,178,152,167,100,40,74,199,186,26,73,200,127,30,124,193,84,184,36,103,149,153,9,54,154,133,95,45,198,79,157,64,122,59,71,48,177,82,35,14,176,16,108,111,6,168,31,134,164,136,72,98], a = 29, b = 98, x = 80))   # -1
+print(so.minimumJumps(forbidden = [14,4,18,1,15], a = 3, b = 15, x = 9))   # 3
+print(so.minimumJumps(forbidden = [1,6,2,14,5,17,4], a = 16, b = 9, x = 7))   # 2
+
+
+
+
+
+
---
+++ all-code/1700-1799/1761minTrioDegree.py
@@ -0,0 +1,75 @@
+# 给你一个无向图，整数 n 表示图中节点的数目，edges 数组表示图中的边，其中 edges[i] = [ui, vi] ，表示 ui 和 vi 之间有一条无向边。
+#
+# 一个 连通三元组 指的是 三个 节点组成的集合且这三个点之间 两两 有边。
+#
+# 连通三元组的度数 是所有满足此条件的边的数目：一个顶点在这个三元组内，而另一个顶点不在这个三元组内。
+#
+# 请你返回所有连通三元组中度数的 最小值 ，如果图中没有连通三元组，那么返回 -1 。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：n = 6, edges = [[1,2],[1,3],[3,2],[4,1],[5,2],[3,6]]
+# 输出：3
+# 解释：只有一个三元组 [1,2,3] 。构成度数的边在上图中已被加粗。
+# 示例 2：
+#
+#
+# 输入：n = 7, edges = [[1,3],[4,1],[4,3],[2,5],[5,6],[6,7],[7,5],[2,6]]
+# 输出：0
+# 解释：有 3 个三元组：
+# 1) [1,4,3]，度数为 0 。
+# 2) [2,5,6]，度数为 2 。
+# 3) [5,6,7]，度数为 2 。
+#
+#
+# 提示：
+#
+# 2 <= n <= 400
+# edges[i].length == 2
+# 1 <= edges.length <= n * (n-1) / 2
+# 1 <= ui, vi <= n
+# ui != vi
+# 图中没有重复的边。
+import bisect
+from cmath import inf
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
+        deg = [0] * n
+        g = defaultdict(list)
+        s = set((x - 1, y - 1) for x, y in edges)
+        for x, y in edges:
+            x, y = sorted([x, y])
+            deg[x - 1] += 1
+            deg[y - 1] += 1
+            g[x - 1].append(y - 1)
+        ans = inf
+        for i in range(n):
+            li = g[i]
+            m = len(g[i])
+            for j in range(m):
+                nj = li[j]
+                for k in range(j + 1, m):
+                    nk = li[k]
+                    if (nj, nk) in s or (nk, nj) in s:
+                        ans = min(ans, deg[i] + deg[nj] + deg[nk] - 6)
+        return ans if ans < inf else -1
+
+
+so = Solution()
+
+print(so.minTrioDegree(n = 6, edges = [[1,2],[1,3],[3,2],[4,1],[5,2],[3,6]]))
+print(so.minTrioDegree(n = 6, edges = [[6,5],[4,3],[5,1],[1,4],[2,3],[4,5],[2,6],[1,3]]))
+print(so.minTrioDegree(n = 17, edges = [[12,10],[12,16],[4,9],[4,6],[14,1],[9,2],[17,6],[17,12],[8,9],[11,14],[13,5],[8,15],[13,11],[15,11],[15,14],[6,8],[12,15],[14,12],[9,1],[9,10],[10,5],[1,11],[2,10],[15,1],[7,9],[14,2],[4,1],[17,7],[3,17],[8,1],[17,13],[10,13],[8,13],[1,7],[2,6],[13,6],[7,2],[1,16],[6,3],[6,9],[16,17],[7,14]]))
+
+print(so.minTrioDegree(n = 7, edges = [[1,3],[4,1],[4,3],[2,5],[5,6],[6,7],[7,5],[2,6]]))
+
+
+
+
