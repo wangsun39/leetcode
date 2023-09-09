c0c6c700aaafbcc7d0178f7f9f3eb706be5584a8
wangsun <825678776@qq.com>
2023-09-09 21:48:23 +0800

    add 2846

---
+++ all-code/2800-2899/2846minOperationsQueries.py
@@ -0,0 +1,240 @@
+# 现有一棵由 n 个节点组成的无向树，节点按从 0 到 n - 1 编号。给你一个整数 n 和一个长度为 n - 1 的二维整数数组 edges ，其中 edges[i] = [ui, vi, wi] 表示树中存在一条位于节点 ui 和节点 vi 之间、权重为 wi 的边。
+#
+# 另给你一个长度为 m 的二维整数数组 queries ，其中 queries[i] = [ai, bi] 。对于每条查询，请你找出使从 ai 到 bi 路径上每条边的权重相等所需的 最小操作次数 。在一次操作中，你可以选择树上的任意一条边，并将其权重更改为任意值。
+#
+# 注意：
+#
+# 查询之间 相互独立 的，这意味着每条新的查询时，树都会回到 初始状态 。
+# 从 ai 到 bi的路径是一个由 不同 节点组成的序列，从节点 ai 开始，到节点 bi 结束，且序列中相邻的两个节点在树中共享一条边。
+# 返回一个长度为 m 的数组 answer ，其中 answer[i] 是第 i 条查询的答案。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：n = 7, edges = [[0,1,1],[1,2,1],[2,3,1],[3,4,2],[4,5,2],[5,6,2]], queries = [[0,3],[3,6],[2,6],[0,6]]
+# 输出：[0,0,1,3]
+# 解释：第 1 条查询，从节点 0 到节点 3 的路径中的所有边的权重都是 1 。因此，答案为 0 。
+# 第 2 条查询，从节点 3 到节点 6 的路径中的所有边的权重都是 2 。因此，答案为 0 。
+# 第 3 条查询，将边 [2,3] 的权重变更为 2 。在这次操作之后，从节点 2 到节点 6 的路径中的所有边的权重都是 2 。因此，答案为 1 。
+# 第 4 条查询，将边 [0,1]、[1,2]、[2,3] 的权重变更为 2 。在这次操作之后，从节点 0 到节点 6 的路径中的所有边的权重都是 2 。因此，答案为 3 。
+# 对于每条查询 queries[i] ，可以证明 answer[i] 是使从 ai 到 bi 的路径中的所有边的权重相等的最小操作次数。
+# 示例 2：
+#
+#
+# 输入：n = 8, edges = [[1,2,6],[1,3,4],[2,4,6],[2,5,3],[3,6,6],[3,0,8],[7,0,2]], queries = [[4,6],[0,4],[6,5],[7,4]]
+# 输出：[1,2,2,3]
+# 解释：第 1 条查询，将边 [1,3] 的权重变更为 6 。在这次操作之后，从节点 4 到节点 6 的路径中的所有边的权重都是 6 。因此，答案为 1 。
+# 第 2 条查询，将边 [0,3]、[3,1] 的权重变更为 6 。在这次操作之后，从节点 0 到节点 4 的路径中的所有边的权重都是 6 。因此，答案为 2 。
+# 第 3 条查询，将边 [1,3]、[5,2] 的权重变更为 6 。在这次操作之后，从节点 6 到节点 5 的路径中的所有边的权重都是 6 。因此，答案为 2 。
+# 第 4 条查询，将边 [0,7]、[0,3]、[1,3] 的权重变更为 6 。在这次操作之后，从节点 7 到节点 4 的路径中的所有边的权重都是 6 。因此，答案为 3 。
+# 对于每条查询 queries[i] ，可以证明 answer[i] 是使从 ai 到 bi 的路径中的所有边的权重相等的最小操作次数。
+#
+#
+# 提示：
+#
+# 1 <= n <= 104
+# edges.length == n - 1
+# edges[i].length == 3
+# 0 <= ui, vi < n
+# 1 <= wi <= 26
+# 生成的输入满足 edges 表示一棵有效的树
+# 1 <= queries.length == m <= 2 * 104
+# queries[i].length == 2
+# 0 <= ai, bi < n
+import math
+from typing import List
+from typing import Optional
+from cmath import *
+from collections import deque
+# de = deque([1, 2, 3])
+# de.append(4)
+# de.appendleft(6)
+# de.pop()
+# de.popleft()
+from itertools import pairwise, accumulate
+# list(accumulate(nums))  数组前缀和
+# Definition for a binary tree node.
+from collections import Counter
+from collections import defaultdict
+# d = Counter(list1)
+# print(c.most_common(2)) # n = 2
+#  [('c', 3), ('b', 2)]
+
+# d = defaultdict(int)
+# from math import *
+import random
+# random.uniform(a, b)，用于生成一个指定范围内的随机浮点数，闭区间
+# randint和randrange的区别：
+# randint 产生的随机数区间是包含左右极限的，也就是说左右都是闭区间的[1, n]，能取到1和n。
+# 而 randrange 产生的随机数区间只包含左极限，也就是左闭右开的[1, n)，1能取到，而n取不到。
+
+# 浮点数： price = "{:.02f}".format(price)
+# newword = float(word[1:]) * (100 - discount) / 100
+# newword = "%.2f" % newword
+# a.isalpha()  # 判断字符串中是否所有的字符都是字母
+# a.isdigit() # 判断字符串中是否所有的字符都是整数
+# a.isalnum()  # 判断字符串中是否所有的字符都是字母or数字
+# a.isspace()  # 判断字符串中是否所有的字符都是空白符
+# a.swapcase()  # 转换大小写
+
+from bisect import *
+# bisect_right：
+# 若序列a中存在与x相同的元素，则返回x相等元素右侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x左侧距离最近元素插入点的索引位置
+# k = bisect_left(a, x) - 1 # k 表示 < x 的最大下标， 不存在: k == -1
+# k = bisect_right(a, x) - 1 # k 表示 <= x 的最大下标， 不存在: k == -1
+# k = bisect_right(a, x) # k 表示 > x 的最小下标， 不存在: k == n
+# k = bisect_left(a, x)  # k 表示 >= x 的最小下标， 不存在: k == n
+
+# pos = bisect.bisect_right(left, tail)
+# bisect_left：
+# 若序列a中存在与x相同的元素，则返回x相等元素左侧插入点的索引位置
+# 若序列a中不存在与x相同的元素，则返回与x右侧距离最近元素插入点的索引位置
+from heapq import *
+# heap.heapify(nums) # 小顶堆
+# heapq.heappop() 函数弹出堆中最小值
+# heapq.heappush(nums, 1)
+# heapq.heapreplace(heap, item)  删除最小值并添加新值
+# 如果需要获取堆中最大或最小的范围值，则可以使用heapq.nlargest() 或heapq.nsmallest() 函数  这2个性能很差
+
+# Map = [['U' for _ in range(n)] for _ in range(m)]
+# Map = [['U'] * n for _ in range(m)]
+
+from functools import lru_cache, cache
+from typing import List, Tuple
+# @lru_cache(None)
+
+# x / y 上取整 (x + y - 1) // y
+# x / y 下取整 x // y
+# x / y 四舍五入 int(x / y + 0.5)
+
+import string
+# string.digits  表示 0123456789
+# string.letters：包含所有字母(大写或小写字符串，在python3.0中，使用string.ascii-letters代替)
+# string.ascii_lowercase：包含所有小写字母的字符串
+# string.ascii_uppercase：包含所有大写字母的字符串
+# string.printable：包含所有可打印字符的字符串
+# string.punctuation：包含所有标点的字符串
+# string.uppercase：包含所有大写字母的字符串
+
+# f-string用法
+# name = 'sun'
+# f"Hello, my name is {name}"
+
+from itertools import accumulate
+# s = list(accumulate(nums, initial=0))  # 计算前缀和
+
+from sortedcontainers import SortedList, SortedDict, SortedSet
+# sl = SortedList()
+# sl.add(value) 添加新元素，并排序。时间复杂度O(log(n)).
+# sl.update(iterable) 对添加的可迭代的所有元素排序。时间复杂度O(k*log(n)).
+# sl.clear() 移除所有元素。时间复杂度O(n).
+# sl.discard(value) 移除一个值元素，如果元素不存在，不报错。时间复杂度O(log(n)).
+# sl.remove(value) 移除一个值元素，如果元素不存在，报错ValueError。时间复杂度O(log(n)).
+# sl.pop(index=-1) 移除一个指定下标元素，如果有序序列为空或者下标超限，报错IndexError.
+# sl.bisect_left(value)
+# sl.bisect_right(value)
+# sl.count(value)
+# sl.index(value, start=None, Stop=None) 查找索引范围[start,stop）内第一次出现value的索引，如果value不存在，报错ValueError.
+
+# sd = SortedDict({'a': 1, 'b': 2, 'c': 3})
+# skv = sd.keys()  这个是有序的
+
+# ss = SortedSet()
+# ss.add(value)
+# ss.pop()
+# ss.pop(value)
+# ss.remove(value)
+# ss.remove(value)
+
+
+# 前缀和
+# 左闭右开区间 [left,right) 来表示从 nums[left] 到 nums[right−1] 的子数组，
+# 此时子数组的和为 s[right]−s[left]，子数组的长度为 right−left。
+# s = list(accumulate(nums, initial=0))
+
+# dir = [[-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1], [0, -1], [0, 1]]
+# dir = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+# nums = [[7,2,1],[6,4,2],[6,5,3],[3,2,1]]
+# list(zip(nums))  # [([7, 2, 1],), ([6, 4, 2],), ([6, 5, 3],), ([3, 2, 1],)]   合并
+# list(zip(*nums))  # [(7, 6, 6, 3), (2, 4, 5, 2), (1, 2, 3, 1)]    转置
+
+class Solution:
+    def minOperationsQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        g = defaultdict(list)
+        w = {}
+        for x, y, z in edges:
+            g[x].append(y)
+            g[y].append(x)
+            w[(x, y)] = w[(y, x)] = z - 1
+        dep = [0] * n  # 节点深度
+
+        m = int(math.log(n, 2)) + 1
+        ac = [[0] * m for _ in range(n)] # ac[i][j] 表示第i个节点的2^j个祖先
+        cnt = [[[0] * 26 for _ in range(m)] for _ in range(n)]  # cnt[i][j][k]  第i个点到它的第2^i个祖先之间权重为k出现的次数
+
+        def dfs(x, p):
+            for y in g[x]:
+                if y == p: continue
+                dep[y] = dep[x] + 1
+                ac[y][0] = x
+                cnt[y][0][w[(x, y)]] = 1
+                dfs(y, x)
+        dfs(0, -1)
+
+        for j in range(1, m):
+            for i in range(n):
+                f = ac[i][j - 1]  # i的第2^(j-1)个祖先
+                if f == 0: continue
+                ac[i][j] = ac[f][j - 1]
+                for k in range(26):
+                    cnt[i][j][k] = cnt[i][j - 1][k] + cnt[f][j - 1][k]
+
+        def lca(x0, y0):
+            if dep[x0] > dep[y0]:
+                x0, y0 = y0, x0
+            x, y = x0, y0
+            counter = [0] * 26
+            diff = dep[y] - dep[x]
+            i = 0
+            while diff:
+                if diff & 1:
+                    for j in range(26):
+                        counter[j] += cnt[y][i][j]
+                    y = ac[y][i]
+                i += 1
+                diff >>= 1
+            if x == y:
+                t = x
+            else:
+                for i in range(m - 1, -1, -1):
+                    if ac[x][i] == ac[y][i]:
+                        continue
+                    for j in range(26):
+                        counter[j] += (cnt[x][i][j] + cnt[y][i][j])
+                    x, y = ac[x][i], ac[y][i]
+                t = ac[x][0]  # lca 节点
+                wx, wy = w[(x, ac[x][0])], w[(y, ac[y][0])]
+                counter[wx] += cnt[x][0][wx]
+                counter[wy] += cnt[y][0][wy]
+            dxy = dep[x0] + dep[y0] - dep[t] * 2  # x,y 的距离
+            return dxy - max(counter)
+
+        ans = [0] * len(queries)
+        for i, [x, y] in enumerate(queries):
+            ans[i] = lca(x, y)
+
+        return ans
+
+
+so = Solution()
+print(so.minOperationsQueries(n = 6, edges = [[4,3,2],[5,3,2],[1,4,5],[0,4,2],[2,1,1]], queries = [[5,2],[0,4],[2,1],[5,4],[2,0],[4,2],[4,5],[3,3],[5,0],[3,5]]))
+print(so.minOperationsQueries(n = 8, edges = [[1,2,6],[1,3,4],[2,4,6],[2,5,3],[3,6,6],[3,0,8],[7,0,2]], queries = [[4,6],[0,4],[6,5],[7,4]]))
+print(so.minOperationsQueries(n = 7, edges = [[0,1,1],[1,2,1],[2,3,1],[3,4,2],[4,5,2],[5,6,2]], queries = [[0,3],[3,6],[2,6],[0,6]]))
+
+
+
+
