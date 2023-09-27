e8cbd4b0024fef0ea42a0775373f5c6731e3b195
wangsun <825678776@qq.com>
2023-09-27 23:06:50 +0800

    add daily problem 2023/9/27

---
+++ all-code/0-100/034searchRange.py
@@ -0,0 +1,45 @@
+# 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+#
+# 如果数组中不存在目标值 target，返回 [-1, -1]。
+#
+# 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+#
+#
+#
+# 示例 1：
+#
+# 输入：nums = [5,7,7,8,8,10], target = 8
+# 输出：[3,4]
+# 示例 2：
+#
+# 输入：nums = [5,7,7,8,8,10], target = 6
+# 输出：[-1,-1]
+# 示例 3：
+#
+# 输入：nums = [], target = 0
+# 输出：[-1,-1]
+#
+#
+# 提示：
+#
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums 是一个非递减数组
+# -109 <= target <= 109
+from bisect import *
+from collections import defaultdict
+from typing import List
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0 or target < nums[0] or target > nums[-1]:
+            return [-1, -1]
+        p1, p2 = bisect_left(nums, target), bisect_right(nums, target)
+        if nums[p1] != target:
+            return [-1, -1]
+        return [p1, p2 - 1]
+
+
+so = Solution()
+print(so.searchRange(nums = [5,7,7,8,8,10], target = 8))
+
+
