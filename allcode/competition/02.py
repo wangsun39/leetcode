1fa968086e411e3b2f155cb822946b31226e4ee6
wangsun <825678776@qq.com>
2023-10-15 19:31:51 +0800

    add weekly contest 367

--- allcode/competition/02.py
+++ allcode/competition/02.py
@@ -3,12 +3,61 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def shortestBeautifulSubstring1(self, s: str, k: int) -> str:
+        n = len(s)
+        if k == 1:
+            return '1' if '1' in s else ''
+        cur = 1 if '1' in s else 0
+        start = 0
+        mil = inf
+        miv = ''
+        for i, x in enumerate(s[1:], 1):
+            if x == '0':
+                continue
+            cur += 1
+            if cur < k:
+                continue
+            if i - start < mil:
+                mil = i - start
+                miv = s[i + 1 - mil: i + 1]
+            elif i - start == mil:
+                if miv > s[i + 1 - mil: i + 1]:
+                    miv = s[i + 1 - mil: i + 1]
+            start += 1
+            while start <= i and s[start] == '0':
+                start += 1
+            cur -= 1
+        return miv
+
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        n = len(s)
+        mil = inf
+        miv = ''
+        if k == 1:
+            return '1' if '1' in s else ''
+        for i in range(n):
+            # if i == '0': continue
+            cnt = 0
+            for j in range(i, n):
+                if s[j] == '1':
+                    cnt += 1
+                    if cnt == k:
+                        if j - i + 1 < mil:
+                            miv = s[i: j + 1]
+                            mil = j - i + 1
+                        elif j - i + 1 == mil:
+                            miv = min(miv, s[i: j + 1])
+                        break
+        return miv
+
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.shortestBeautifulSubstring("1100100101011001001", 7))
+print(so.shortestBeautifulSubstring("11000111", 1))
+print(so.shortestBeautifulSubstring(s = "100011001", k = 3))
+print(so.shortestBeautifulSubstring(s = "1011", k = 2))
+print(so.shortestBeautifulSubstring(s = "000", k = 1))
 
 
 
--- allcode/competition/04.py
+++ allcode/competition/04.py
@@ -3,12 +3,51 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        MOD = 12345
+        product = 1
+        r, c = len(grid), len(grid[0])
+        for i in range(r):
+            for j in range(c):
+                product *= grid[i][j]
+                product %= MOD
+        ans = [[0] * c for _ in range(r)]
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] % MOD:
+                    x = product // (grid[i][j] % MOD)
+                else:
+                    x = product
+                ans[i][j] = x
+
+        # nums = []
+        # for x in grid:
+        #     nums += x
+        # n = len(nums)
+        #
+        # @cache
+        # def dfs(i, j):
+        #     if i > j:
+        #         return 1
+        #     if i == j:
+        #         return nums[i] % MOD
+        #     mid = (i + j) // 2
+        #     return (dfs(i, mid) * dfs(mid + 1, j)) % MOD
+        #
+        # res = [0] * (r * c)
+        # for i in range(r * c):
+        #     res[i] = dfs(0, i - 1) * dfs(i + 1, n - 1)
+        # ans = [[0] * c for _ in range(r)]
+        # for i in range(r):
+        #     for j in range(c):
+        #         ans[i][j] = res[i * c + j]
+        return ans
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.constructProductMatrix(grid = [[12345],[2],[1]]))
+print(so.constructProductMatrix(grid = [[3,2,5],[6,4,3],[6,3,1]]))
+print(so.constructProductMatrix(grid = [[1,2],[3,4]]))
 
 
 
