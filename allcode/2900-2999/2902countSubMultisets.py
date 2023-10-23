0469f0792aab7185d7a1662ee450d89bb4efa5ad
wangsun <825678776@qq.com>
2023-10-23 20:22:42 +0800

    add weekly contest 368

--- allcode/2900-2999/2902countSubMultisets.py
+++ allcode/2900-2999/2902countSubMultisets.py
@@ -39,7 +39,8 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def countSubMultisets(self, nums: List[int], l: int, r: int) -> int:
+    def countSubMultisets1(self, nums: List[int], l: int, r: int) -> int:
+        # 性能不够 O(N^3)
         MOD = 10 ** 9 + 7
         counter = Counter(nums)
         counter = [[k, v] for k, v in counter.items()]
@@ -60,11 +61,34 @@ class Solution:
         v1, v2 = dfs(0, l - 1), dfs(0, r)
         return (v2 - v1 + MOD) % MOD
 
+    def countSubMultisets(self, nums: List[int], l: int, r: int) -> int:
+        MOD = 10 ** 9 + 7
+        counter = Counter(nums)
+        counter = [[k, v] for k, v in counter.items()]
+        n = len(counter)
+
+        @cache
+        def dfs(i, j):  # 前i个数，组成j的各种组合数
+            # dp[i][j] = dp[i-1][j]+dp[i-1][j-k]+dp[i-1][j-2k]+...+dp[i-1][j-vk]
+            # dp[i][j-k] =          dp[i-1][j-k]+dp[i-1][j-2k]+...+dp[i-1][j-vk]+dp[i-1][j-(v+1)k]
+            if j < 0 or i < 0: return 0
+            k, v = counter[i]
+            if j == 0: return v + 1 if k == 0 else 1
+            if k == 0: return dfs(i - 1, j)
+            if i == 0: return j % k <= v
+            if j < k:
+                return dfs(i - 1, j) + 1
+            if (v + 1) * k <= j:
+                return (dfs(i, j - k) + dfs(i - 1, j) - dfs(i - 1, j - (v + 1) * k)) % MOD
+            return (dfs(i, j - k) + dfs(i - 1, j)) % MOD
+
+        return sum(dfs(n - 1, i) for i in range(l, r + 1)) % MOD
+
 
 so = Solution()
+print(so.countSubMultisets(nums = [1,2], l = 3, r = 3))
 print(so.countSubMultisets(nums = [0,0,0,0], l = 0, r = 0))
 print(so.countSubMultisets(nums = [2,1,4,2,7], l = 1, r = 5))
-print(so.countSubMultisets(nums = [1,2,2,3], l = 6, r = 6))
 print(so.countSubMultisets(nums = [1,2,1,3,5,2], l = 3, r = 5))
 
 
--- allcode/competition/03.py
+++ allcode/competition/03.py
@@ -1,14 +1,84 @@
-
+# 给你一个长度为 n 下标从 0 开始的整数数组 nums 。
+#
+# 我们想将下标进行分组，使得 [0, n - 1] 内所有下标 i 都 恰好 被分到其中一组。
+#
+# 如果以下条件成立，我们说这个分组方案是合法的：
+#
+# 对于每个组 g ，同一组内所有下标在 nums 中对应的数值都相等。
+# 对于任意两个组 g1 和 g2 ，两个组中 下标数量 的 差值不超过 1 。
+# 请你返回一个整数，表示得到一个合法分组方案的 最少 组数。
+#
+#
+#
+# 示例 1：
+#
+# 输入：nums = [3,2,3,2,3]
+# 输出：2
+# 解释：一个得到 2 个分组的方案如下，中括号内的数字都是下标：
+# 组 1 -> [0,2,4]
+# 组 2 -> [1,3]
+# 所有下标都只属于一个组。
+# 组 1 中，nums[0] == nums[2] == nums[4] ，所有下标对应的数值都相等。
+# 组 2 中，nums[1] == nums[3] ，所有下标对应的数值都相等。
+# 组 1 中下标数目为 3 ，组 2 中下标数目为 2 。
+# 两者之差不超过 1 。
+# 无法得到一个小于 2 组的答案，因为如果只有 1 组，组内所有下标对应的数值都要相等。
+# 所以答案为 2 。
+# 示例 2：
+#
+# 输入：nums = [10,10,10,3,1,1]
+# 输出：4
+# 解释：一个得到 2 个分组的方案如下，中括号内的数字都是下标：
+# 组 1 -> [0]
+# 组 2 -> [1,2]
+# 组 3 -> [3]
+# 组 4 -> [4,5]
+# 分组方案满足题目要求的两个条件。
+# 无法得到一个小于 4 组的答案。
+# 所以答案为 4 。
+#
+#
+# 提示：
+#
+# 1 <= nums.length <= 105
+# 1 <= nums[i] <= 109
 
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def minGroupsForValidAssignment(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        cc = Counter(counter.values())
+        if len(cc) == 1:
+            return len(counter)
+
+        def check(val):
+            ans = 0
+            for x, t in cc.items():
+                if x % val == 0:
+                    ans += (x // val) * t
+                    continue
+                q, r = divmod(x, val)  # x = q * val + r
+                if q >= val - 1 - r:   # x = (q - (val - 1 - r)) * val + (val - 1 - r + 1) * (val - 1)
+                    # ans += (q - (val - 1 - r) + val - 1 - r + 1)  化简为下面的式子
+                    ans += (q + 1) * t
+                else:
+                    return -1
+            return ans
+
+        mx = min(cc.keys()) + 1
+
+        for i in range(mx, 1, -1):
+            res = check(i)
+            if res != -1:
+                return res
+
+
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.minGroupsForValidAssignment([10,10,10,3,1,1]))
+print(so.minGroupsForValidAssignment([3,2,3,2,3]))
 
 
 
