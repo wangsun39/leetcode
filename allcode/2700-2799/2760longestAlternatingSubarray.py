13f89beed63c401d933e5a347fed87b55adc4b39
wangsun <825678776@qq.com>
2023-11-16 21:47:57 +0800

    add daily problem 2023/11/16

--- allcode/2700-2799/2760longestAlternatingSubarray.py
+++ allcode/2700-2799/2760longestAlternatingSubarray.py
@@ -2,7 +2,7 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+    def longestAlternatingSubarray1(self, nums: List[int], threshold: int) -> int:
         n = len(nums)
         ans = 0
         for i, x in enumerate(nums):
@@ -17,6 +17,23 @@ class Solution:
                     break
         return ans
 
+    def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
+        # 2023/11/16  双指针
+        n = len(nums)
+        l = 0
+        ans = 0
+        while l < n:
+            if nums[l] > threshold or nums[l] & 1:
+                l += 1
+                continue
+            r = l + 1
+            while r < n and nums[r] & 1 != nums[r - 1] & 1 and nums[r] <= threshold:
+                r += 1
+            ans = max(ans, r - l)
+            if r >= n:
+                break
+            l = r
+        return ans
 
 so = Solution()
 print(so.longestAlternatingSubarray(nums = [2,2], threshold = 18))
