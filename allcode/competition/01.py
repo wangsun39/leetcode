168ce59b3f4117175ce2556581aa5ccf13c54744
wangsun <825678776@qq.com>
2024-01-07 20:52:46 +0800

    add weekly contest 379

--- allcode/competition/01.py
+++ allcode/competition/01.py
@@ -3,12 +3,16 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        diag = [[x * x + y * y, x, y] for x, y in dimensions]
+        diag.sort(key=lambda x: [x[0], x[1] * x[2]], reverse=True)
+        return diag[0][1] * diag[0][2]
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.areaOfMaxDiagonal(dimensions = [[9,3],[8,6]]))
+print(so.areaOfMaxDiagonal(dimensions = [[3,4],[4,3]]))
+print(so.areaOfMaxDiagonal(dimensions = [[9,3],[8,6]]))
 
 
 
--- allcode/competition/03.py
+++ allcode/competition/03.py
@@ -3,12 +3,36 @@
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def removeDigit(self) -> str:
-        pass
+    def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+        s1, s2 = set(nums1), set(nums2)
+        s = s1 & s2
+        ans1 = set()
+        for x in s1:
+            if x not in s2:
+                ans1.add(x)
+            if len(ans1) >= n // 2:
+                break
+        ans2 = set()
+        for x in s2:
+            if x not in s1:
+                ans2.add(x)
+            if len(ans2) >= n // 2:
+                break
+        ans = ans1 | ans2
+        for x in s:
+            if len(ans) >= n:
+                break
+            ans.add(x)
+        return len(ans)
+
+
 
 
 so = Solution()
-print(so.removeDigit())
+print(so.maximumSetSize(nums1 = [1,1,2,2,3,3], nums2 = [4,4,5,5,6,6]))
+print(so.maximumSetSize(nums1 = [1,2,3,4,5,6], nums2 = [2,3,2,3,2,3]))
+print(so.maximumSetSize(nums1 = [1,2,1,2], nums2 = [1,1,1,1]))
 
 
 
