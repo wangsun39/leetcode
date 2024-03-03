989f2d3e22ba23bbeffe7f8a8a639e991c2c0f40
wangsun <825678776@qq.com>
2024-03-03 19:06:15 +0800

    add weekly contest 387

--- allcode/competition/02.py
+++ allcode/competition/02.py
@@ -1,29 +1,66 @@
-
+# 给你一个下标从 0 开始的整数矩阵 grid 和一个整数 k。
+#
+# 返回包含 grid 左上角元素、元素和小于或等于 k 的 子矩阵 的
+# 数目
+# 。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：grid = [[7,6,3],[6,6,1]], k = 18
+# 输出：4
+# 解释：如上图所示，只有 4 个子矩阵满足：包含 grid 的左上角元素，并且元素和小于或等于 18 。
+# 示例 2：
+#
+#
+# 输入：grid = [[7,2,9],[1,5,0],[2,6,6]], k = 20
+# 输出：6
+# 解释：如上图所示，只有 6 个子矩阵满足：包含 grid 的左上角元素，并且元素和小于或等于 20 。
+#
+#
+# 提示：
+#
+# m == grid.length
+# n == grid[i].length
+# 1 <= n, m <= 1000
+# 0 <= grid[i][j] <= 1000
+# 1 <= k <= 109
 
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
-        n = len(bottomLeft)
-        ans = 0
-        for i in range(n):
-            x1, y1 = bottomLeft[i]
-            x2, y2 = topRight[i]
-            for j in range(i + 1, n):
-                u1, v1 = bottomLeft[j]
-                u2, v2 = topRight[j]
-                if x1 >= u2 or u1 >= x2 or y1 >= v2 or v1 >= y2:
-                    continue
-                a = sorted([x1, x2, u1, u2])
-                b = sorted([y1, y2, v1, v2])
-                ans = max(ans, min(a[2] - a[1], b[2] - b[1]))
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        r, c = len(grid), len(grid[0])
+        s = [[0] * c for _ in range(r)]
+        s[0][0] = grid[0][0]
+        if s[0][0] > k:
+            return 0
+        ans = 1
+        for i in range(1, c):
+            s[0][i] = s[0][i - 1] + grid[0][i]
+            if s[0][i] <= k:
+                ans += 1
+            else:
+                break
+        for i in range(1, r):
+            row = 0
+            for j in range(c):
+                row += grid[i][j]
+                s[i][j] = s[i - 1][j] + row
+                if s[i][j] <= k:
+                    ans += 1
+                else:
+                    break
         return ans
 
 
+
 so = Solution()
-print(so.largestSquareArea(bottomLeft = [[1,1],[2,2],[3,1]], topRight = [[3,3],[4,4],[6,6]]))
-print(so.largestSquareArea(bottomLeft = [[1,1],[2,2],[1,2]], topRight = [[3,3],[4,4],[3,4]]))
-print(so.largestSquareArea(bottomLeft = [[1,1],[3,3],[3,1]], topRight = [[2,2],[4,4],[4,2]]))
+print(so.countSubmatrices(grid = [[7,6,3],[6,6,1]], k = 18))
+print(so.countSubmatrices(grid = [[7,2,9],[1,5,0],[2,6,6]], k = 20))
+print(so.countSubmatrices(grid = [[7,6,3],[6,6,1]], k = 18))
 
 
 
--- allcode/competition/03.py
+++ allcode/competition/03.py
@@ -1,36 +1,73 @@
-
+# 给你一个下标从 0 开始、大小为 n x n 的矩阵 grid ，其中 n 为奇数，且 grid[r][c] 的值为 0 、1 或 2 。
+#
+# 如果一个单元格属于以下三条线中的任一一条，我们就认为它是字母 Y 的一部分：
+#
+# 从左上角单元格开始到矩阵中心单元格结束的对角线。
+# 从右上角单元格开始到矩阵中心单元格结束的对角线。
+# 从中心单元格开始到矩阵底部边界结束的垂直线。
+# 当且仅当满足以下全部条件时，可以判定矩阵上写有字母 Y ：
+#
+# 属于 Y 的所有单元格的值相等。
+# 不属于 Y 的所有单元格的值相等。
+# 属于 Y 的单元格的值与不属于Y的单元格的值不同。
+# 每次操作你可以将任意单元格的值改变为 0 、1 或 2 。返回在矩阵上写出字母 Y 所需的 最少 操作次数。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：grid = [[1,2,2],[1,1,0],[0,1,0]]
+# 输出：3
+# 解释：将在矩阵上写出字母 Y 需要执行的操作用蓝色高亮显示。操作后，所有属于 Y 的单元格（加粗显示）的值都为 1 ，而不属于 Y 的单元格的值都为 0 。
+# 可以证明，写出 Y 至少需要进行 3 次操作。
+# 示例 2：
+#
+#
+# 输入：grid = [[0,1,0,1,0],[2,1,0,1,2],[2,2,2,0,1],[2,2,2,2,2],[2,1,2,2,2]]
+# 输出：12
+# 解释：将在矩阵上写出字母 Y 需要执行的操作用蓝色高亮显示。操作后，所有属于 Y 的单元格（加粗显示）的值都为 0 ，而不属于 Y 的单元格的值都为 2 。
+# 可以证明，写出 Y 至少需要进行 12 次操作。
+#
+#
+# 提示：
+#
+# 3 <= n <= 49
+# n == grid.length == grid[i].length
+# 0 <= grid[i][j] <= 2
+# n 为奇数。
 
 from leetcode.allcode.competition.mypackage import *
 
 class Solution:
-    def earliestSecondToMarkIndices(self, nums: List[int], changeIndices: List[int]) -> int:
-        n = len(nums)
-        m = len(changeIndices)
-        s = sum(nums)
+    def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        mid = n // 2
+        cy = Counter()
+        ncy = Counter()
+        for i in range(n):
+            for j in range(n):
+                v = grid[i][j]
+                if i <= mid and (i == j or i + j == n - 1):
+                    cy[v] += 1
+                elif i > mid and j == mid:
+                    cy[v] += 1
+                else:
+                    ncy[v] += 1
+        ny, nny = sum(cy.values()), sum(ncy.values())
+        ans = inf
+        for i in range(3):
+            for j in range(3):
+                if i != j:
+                    cnt = ny - cy[i] + nny - ncy[j]
+                    ans = min(ans, cnt)
+        return ans
 
-        def check(t):  # 检查时间t内是否能完成
-            left = s
-            vis = set()
-            for i in range(t, 0, -1):
-                idx = changeIndices[i - 1] - 1
-                if idx not in vis:
-                    v = nums[idx]
-                    left -= v
-                    vis.add(idx)
-                if left + n - len(vis) > i - 1: return False
-            return left == 0 and len(vis) == n
-
-        for i in range(s + n, m + 1):
-            if check(i):
-                return i
-        return -1
 
 
 so = Solution()
-print(so.earliestSecondToMarkIndices([1,0,1,2], [3,1,1,4,4,4,4,2,2,1,3,4]))  # 11
-print(so.earliestSecondToMarkIndices(nums = [2,2,0], changeIndices = [2,2,2,2,3,2,2,1]))
-print(so.earliestSecondToMarkIndices(nums = [1,3], changeIndices = [1,1,1,2,1,1,1]))
-print(so.earliestSecondToMarkIndices(nums = [0,1], changeIndices = [2,2,2]))
+print(so.minimumOperationsToWriteY([[1,2,2],[1,1,0],[0,1,0]]))
+print(so.minimumOperationsToWriteY([[0,1,0,1,0],[2,1,0,1,2],[2,2,2,0,1],[2,2,2,2,2],[2,1,2,2,2]]))
 
 
 
