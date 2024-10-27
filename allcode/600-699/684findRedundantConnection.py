b3929badb9230e673c104c700f5e65f4cc4a9314
wangsun <825678776@qq.com>
2024-10-27 09:46:39 +0800

    daily problem 2024/10/27

--- allcode/600-699/684findRedundantConnection.py
+++ allcode/600-699/684findRedundantConnection.py
@@ -1,3 +1,37 @@
+# 树可以看成是一个连通且 无环 的 无向 图。
+#
+# 给定往一棵 n 个节点 (节点值 1～n) 的树中添加一条边后的图。添加的边的两个顶点包含在 1 到 n 中间，且这条附加的边不属于树中已存在的边。图的信息记录于长度为 n 的二维数组 edges ，edges[i] = [ai, bi] 表示图中在 ai 和 bi 之间存在一条边。
+#
+# 请找出一条可以删去的边，删除后可使得剩余部分是一个有着 n 个节点的树。如果有多个答案，则返回数组 edges 中最后出现的那个。
+#
+#
+#
+# 示例 1：
+#
+#
+#
+# 输入: edges = [[1,2], [1,3], [2,3]]
+# 输出: [2,3]
+# 示例 2：
+#
+#
+#
+# 输入: edges = [[1,2], [2,3], [3,4], [1,4], [1,5]]
+# 输出: [1,4]
+#
+#
+# 提示:
+#
+# n == edges.length
+# 3 <= n <= 1000
+# edges[i].length == 2
+# 1 <= ai < bi <= edges.length
+# ai != bi
+# edges 中无重复元素
+# 给定的图是连通的
+
+from leetcode.allcode.competition.mypackage import *
+
 class Solution:
     def findRedundantConnection(self, edges):
         self.d = {}
@@ -60,7 +94,7 @@ class UnionFind:
         return self.find(u) == self.find(v)
 
 class Solution1:
-    def findRedundantConnection(self, edges):
+    def findRedundantConnection1(self, edges):
         uf = UnionFind(len(edges))
         for e in edges:
             print(uf.ids)
@@ -68,6 +102,34 @@ class Solution1:
             if uf.connect(u, v):
                 return u, v
             uf.union(u, v)
+
+    def findRedundantConnection(self, edges):
+        g = defaultdict(list)
+        n = len(edges)
+        deg = [0] * (n + 1)
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+            deg[x] += 1
+            deg[y] += 1
+        dq = deque([i for i in range(n + 1) if deg[i] == 1])
+        left = set(range(1, n + 1))  # 剩余的点
+        for x in dq:
+            left.remove(x)
+            deg[x] = 0
+        while dq:  # 不断删除度为1的点
+            x = dq.popleft()
+            for y in g[x]:
+                if y not in left: continue
+                deg[y] -= 1
+                if deg[y] == 1:
+                    dq.append(y)
+                    left.remove(y)
+        for x, y in edges[::-1]:
+            if x in left and y in left:
+                return [x, y]
+
+
 
 so = Solution1()
 #print(so.findRedundantConnection([[1, 2], [1, 3], [2, 3]]))
